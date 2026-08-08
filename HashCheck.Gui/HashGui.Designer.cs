9f4b1881aa10685d3c6d26d9e694ec47411f1876
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2026-08-09 06:27:53 +1000

    feat(gui): add generate hashes

---
+++ HashCheck.Gui/HashGui.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HashCheck.Gui
+{
+    partial class HashGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HashGui));
+            fileInputPanel = new Panel();
+            filePathLabel = new Label();
+            openFileButton = new Button();
+            hashOutputGridView = new DataGridView();
+            HashAlgo = new DataGridViewTextBoxColumn();
+            Hash = new DataGridViewTextBoxColumn();
+            fileInputPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)hashOutputGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // fileInputPanel
+            // 
+            fileInputPanel.AllowDrop = true;
+            fileInputPanel.BorderStyle = BorderStyle.FixedSingle;
+            fileInputPanel.Controls.Add(filePathLabel);
+            resources.ApplyResources(fileInputPanel, "fileInputPanel");
+            fileInputPanel.Name = "fileInputPanel";
+            fileInputPanel.DragDrop += fileInputPanel_DragDrop;
+            fileInputPanel.DragEnter += fileInputPanel_DragEnter;
+            // 
+            // filePathLabel
+            // 
+            resources.ApplyResources(filePathLabel, "filePathLabel");
+            filePathLabel.Name = "filePathLabel";
+            // 
+            // openFileButton
+            // 
+            resources.ApplyResources(openFileButton, "openFileButton");
+            openFileButton.Name = "openFileButton";
+            openFileButton.UseVisualStyleBackColor = true;
+            openFileButton.Click += openFileButton_Click;
+            // 
+            // hashOutputGridView
+            // 
+            hashOutputGridView.AllowUserToAddRows = false;
+            hashOutputGridView.AllowUserToDeleteRows = false;
+            hashOutputGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            hashOutputGridView.Columns.AddRange(new DataGridViewColumn[] { HashAlgo, Hash });
+            resources.ApplyResources(hashOutputGridView, "hashOutputGridView");
+            hashOutputGridView.Name = "hashOutputGridView";
+            hashOutputGridView.ReadOnly = true;
+            hashOutputGridView.RowHeadersVisible = false;
+            // 
+            // HashAlgo
+            // 
+            resources.ApplyResources(HashAlgo, "HashAlgo");
+            HashAlgo.Name = "HashAlgo";
+            HashAlgo.ReadOnly = true;
+            // 
+            // Hash
+            // 
+            resources.ApplyResources(Hash, "Hash");
+            Hash.Name = "Hash";
+            Hash.ReadOnly = true;
+            // 
+            // HashGui
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(hashOutputGridView);
+            Controls.Add(openFileButton);
+            Controls.Add(fileInputPanel);
+            Name = "HashGui";
+            Load += HashGui_Load;
+            fileInputPanel.ResumeLayout(false);
+            fileInputPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)hashOutputGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel fileInputPanel;
+        private Label filePathLabel;
+        private Button openFileButton;
+        private DataGridView hashOutputGridView;
+        private DataGridViewTextBoxColumn HashAlgo;
+        private DataGridViewTextBoxColumn Hash;
+    }
+}
---
+++ HashCheck.Gui/HashGui.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Forms;
+
+using HashCheck.Core;
+
+namespace HashCheck.Gui
+{
+    public partial class HashGui : Form
+    {
+        private string? _filePath;
+        public HashGui()
+        {
+            InitializeComponent();
+        }
+
+        private void fileInputPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void fileInputPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            string[]? files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string? path = files?[0];
+            if (path == null)
+                return;
+
+            _filePath = path;
+            filePathLabel.Text = _filePath;
+            generateHash();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "All files (*.*)|*.*";
+                dialog.Title = "Select a file to hash";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = dialog.FileName;
+                    _filePath = path;
+                    filePathLabel.Text = path;
+                    generateHash();
+                }
+            }
+        }
+
+        private void generateHash()
+        {
+            Dictionary<HashKind, string> hashes;
+            // try bc might be a dir from the dragdrop or non-exist file
+            try
+            {
+                hashes = Hasher.ComputeAllPath(_filePath);
+            }
+            catch
+            {
+                _filePath = null;
+                filePathLabel.Text = "Drag file here";
+                return;
+            }
+            hashOutputGridView.Rows.Clear();
+            foreach (var hash in hashes)
+            {
+                // size of sha512 doesn't fit in the gui table, and the default renderer doesn't wrap lines with no spaces in them.
+                // TODO: (maybe) insert spaces into the gui
+                if (hash.Key == HashKind.SHA512)
+                    continue;
+                hashOutputGridView.Rows.Add(hash.Key, hash.Value);
+            }
+        }
+
+        private void HashGui_Load(object sender, EventArgs e)
+        {
+            hashOutputGridView.Columns["HashAlgo"]!.Width = 100;
+            hashOutputGridView.Columns["Hash"]!.Width = hashOutputGridView.Width - 100 - 3;
+            hashOutputGridView.Columns["Hash"]!.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            hashOutputGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+        }
+
+        private void fileInputPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
